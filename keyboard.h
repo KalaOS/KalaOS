947b3c3cd672eb0b47c420ecc2bf3f42bd706282
Elie Renneson <118740084+Elie-Renneson@users.noreply.github.com>
2023-11-29 06:51:52 +0000

    Create keyboard.h

---
+++ keyboard.h
@@ -0,0 +1,11 @@
+#ifndef KEYBOARD_DOT_H    /* This is an "include guard" */
+#define KEYBOARD_DOT_H 
+
+
+
+
+unsigned char get_scancode();
+void keyboard_handler();
+void keyboard_handler_test();
+
+#endif
